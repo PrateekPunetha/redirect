1c8d6d7ef0b75a4ebdaa9f800429b14e9db9cf16
prateekpunetha <prateekpunetha@gmail.com>
2020-06-25 13:07:41 +0530

    redirekt: src/handler.ts: Modify thing according to my usage
    Signed-off-by: prateekpunetha <prateekpunetha@gmail.com>

--- src/handler.ts
+++ src/handler.ts
@@ -1,44 +1,17 @@
-const BASE_URL = 'https://msfjarvis.dev/'
-const DOWNLOAD_URL = 'https://download.msfjarvis.dev/'
-const DOWNLOAD_DEST_URL = 'https://dl.msfjarvis.dev/'
-const GITHUB_USERNAME = 'msfjarvis'
-const APS_SLUG = 'Android-Password-Store/Android-Password-Store'
+const BASE_URL = 'https://prateekpunetha.me/'
+const DOWNLOAD_URL = 'https://download.prateekpunetha.me/'
+const DOWNLOAD_DEST_URL = 'https://dl.prateekpunetha.me/'
+const GITHUB_USERNAME = 'prateekpunetha'
 const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}`
-const APS_GITHUB_URL = `https://github.com/${APS_SLUG}`
-const PAYID_DATA = {
-  harsh: {
-    addresses: [
-      {
-        paymentNetwork: 'BTC',
-        environment: 'MAINNET',
-        addressDetailsType: 'CryptoAddressDetails',
-        addressDetails: {
-          address: '1LbHbfTq1ZtE8u3qipCTJTcJYptvzc54aB',
-        },
-      },
-    ],
-    payId: 'harsh$msfjarvis.dev',
-  },
-}
 
 export async function handleRequest(request: Request): Promise<Response> {
-  if (request.headers.get('Accept') == 'application/btc-mainnet+json') {
-    return sendPayID(request)
-  } else if (request.url.startsWith(DOWNLOAD_URL)) {
+  if (request.url.startsWith(DOWNLOAD_URL)) {
     return redirectDownload(request, DOWNLOAD_URL, DOWNLOAD_DEST_URL)
   } else if (request.url.startsWith(BASE_URL)) {
-    return redirectGithub(request, BASE_URL, GITHUB_URL, APS_GITHUB_URL)
+    return redirectGithub(request, BASE_URL, GITHUB_URL)
   } else {
     return fetch(request)
   }
-}
-
-async function sendPayID(request: Request): Promise<Response> {
-  switch (request.url.split('/').reverse()[0]) {
-    case 'harsh':
-      return new Response(JSON.stringify(PAYID_DATA.harsh))
-  }
-  return fetch(request)
 }
 
 async function redirectDownload(
@@ -71,15 +44,6 @@ async function redirectGithub(
             `${githubUrl}/${urlParts[1]}/commit/${urlParts[2]}`,
             301,
           )
-      }
-    case 'ga':
-      switch (urlParts.length) {
-        case 1:
-          return Response.redirect(apsUrl, 301)
-        case 2:
-          return Response.redirect(`${apsUrl}/commit/${urlParts[1]}`, 301)
-        case 3:
-          return Response.redirect(`${apsUrl}/issues/${urlParts[2]}`, 301)
       }
   }
   return fetch(request)
